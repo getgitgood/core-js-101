5abed13d50739ff3877ce980bce6b3ae573ca3be
Kotec <vivalastelmah@gmail.com>
2023-07-03 22:51:51 +0300

    feat: 04-date-tasks done

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -106,8 +106,16 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const localDate = new Date(date.toLocaleString());
+  const [hours, min] = [localDate.getUTCHours(), localDate.getUTCMinutes()];
+  const minDeg = min * 6;
+  const hDeg = (hours % 12) * 30 + (min / 2);
+  let angle = Math.abs(hDeg - minDeg);
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+  return angle * (Math.PI / 180);
 }
 
 
